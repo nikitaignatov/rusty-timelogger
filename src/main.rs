b5918dbbaf7326498238705106bf4db65abc6f57
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2019-11-23 17:06:30 +0100

    Restricted input for time spent to only allow valid duration

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+extern crate humantime;
 use structopt::StructOpt;
+use humantime::Duration;
 
 #[derive(StructOpt, Debug)]
 struct Log {
     /// How much time was spent on this worklog. Examples: is 1h or 1h30m or 30m
-    time_spent: String,
+    time_spent: Duration,
     /// Issue Key for this worklog. Examples: PROJ-1234
     issue: String,
     /// Describe what you have done
