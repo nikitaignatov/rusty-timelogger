ed6f44c0ba26574f5384ae684a5f7b13431d714b
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2019-11-23 18:12:41 +0100

    Added type for IssueKey

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,37 @@
 extern crate humantime;
-use structopt::StructOpt;
 use humantime::Duration;
+use std::num::ParseIntError;
+use std::str::FromStr;
+use structopt::StructOpt;
+
+#[derive(Debug)]
+struct IssueKey {
+    project: String,
+    key: i16,
+}
+
+/// Jira IssueKey parses PROJ-1234 into a struct of IssueKey { project: "PROJ", key: 1234 }
+impl FromStr for IssueKey {
+    type Err = ParseIntError;
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = input.split("-").collect();
+        let project = parts[0].to_string().to_uppercase();
+        let key = parts[1]
+            .parse()
+            .expect(&("Failed to extract key from the issue. ".to_owned() + input));
+        Ok(IssueKey {
+            project: project,
+            key: key,
+        })
+    }
+}
 
 #[derive(StructOpt, Debug)]
 struct Log {
     /// How much time was spent on this worklog. Examples: is 1h or 1h30m or 30m
     time_spent: Duration,
     /// Issue Key for this worklog. Examples: PROJ-1234
-    issue: String,
+    issue: IssueKey,
     /// Describe what you have done
     comment: Option<String>,
 }
