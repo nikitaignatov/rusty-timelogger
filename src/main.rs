dd919ec03eb37ef345a53c48b7a13edea1a121d1
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2019-11-23 16:38:46 +0100

    Added a basic prototype for log command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Log {
+    /// How much time was spent on this worklog. Examples: is 1h or 1h30m or 30m
+    time_spent: String,
+    /// Issue Key for this worklog. Examples: PROJ-1234
+    issue: String,
+    /// Describe what you have done
+    comment: Option<String>,
+}
+
+#[derive(StructOpt, Debug)]
+enum App {
+    Log(Log),
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", App::from_args());
 }
