b86f34be30905ebadfffe207dda76d7bafcf75db
Nikita Ignatov <4703715+nikitaignatov@users.noreply.github.com>
2019-11-24 02:19:54 +0100

    added unit tests for the worklog

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::num::ParseIntError;
 use std::str::FromStr;
 use structopt::StructOpt;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct IssueKey {
     project: String,
     key: i16,
@@ -26,7 +26,7 @@ impl FromStr for IssueKey {
     }
 }
 
-#[derive(StructOpt, Debug)]
+#[derive(StructOpt, PartialEq, Debug)]
 struct Log {
     /// How much time was spent on this worklog. Examples: is 1h or 1h30m or 30m
     time_spent: Duration,
@@ -36,7 +36,7 @@ struct Log {
     comment: Option<String>,
 }
 
-#[derive(StructOpt, Debug)]
+#[derive(StructOpt, PartialEq, Debug)]
 #[structopt(about = "Rusty TimeLogger is a cli for the timelogging")]
 enum App {
     /// Log command allows you to create a work log for a specific issue.
@@ -45,4 +45,61 @@ enum App {
 
 fn main() {
     println!("{:?}", App::from_args());
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate test_case_derive;
+    use super::*;
+    use test_case_derive::test_case;
+
+    #[test_case(vec!["log", "1h", "a-1"],                  None)]
+    #[test_case(vec!["log", "1h", "a-1","test"],           Some("test".to_string()))]
+    #[test_case(vec!["log", "1h", "a-1",""],               Some("".to_string()))]
+    #[test_case(vec!["log", "1h", "a-1","some work log"],  Some("some work log".to_string()))]
+    fn comment(input: Vec<&str>, expeced: Option<String>) {
+        match Log::from_clap(&Log::clap().get_matches_from(input)) {
+            Log {
+                comment,
+                issue,
+                time_spent,
+            } => assert_eq!(comment, expeced),
+        }
+    }
+
+    #[test_case(vec!["log", "1h",       "a-1"],         3600)]
+    #[test_case(vec!["log", "1h30m",    "a-1"],         5400)]
+    #[test_case(vec!["log", "1m",       "a-1"],         60)]
+    fn time_spent(input: Vec<&str>, expeced: u64) {
+        match Log::from_clap(&Log::clap().get_matches_from(input)) {
+            Log {
+                comment,
+                issue,
+                time_spent,
+            } => assert_eq!(
+                time_spent,
+                Duration::from(std::time::Duration::new(expeced, 0))
+            ),
+        }
+    }
+
+    #[test_case(vec!["log", "1h", "a-1"],       "A",1)]
+    #[test_case(vec!["log", "1h", "a-2"],       "A",2)]
+    #[test_case(vec!["log", "1m", "b-0"],       "B",0)]
+    #[test_case(vec!["log", "1m", "proj-1234"], "PROJ",1234)]
+    fn issue_key(input: Vec<&str>, project: &str, key: i16) {
+        match Log::from_clap(&Log::clap().get_matches_from(input)) {
+            Log {
+                comment,
+                issue,
+                time_spent,
+            } => assert_eq!(
+                issue,
+                IssueKey {
+                    project: project.to_string(),
+                    key: key
+                }
+            ),
+        }
+    }
 }
